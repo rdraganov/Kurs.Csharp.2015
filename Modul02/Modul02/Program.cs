5baac78463c87b165274152a3dd8eeba02329699
Румен Драганов <r.draganov@respect93.com>
2015-08-05 18:10:42 +0300

    Модул 02 Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			//Дефиниране на променливи
+
+			//Събира не на числа
+
+			//Писане в конзола
+
+		}
+	}
+}
