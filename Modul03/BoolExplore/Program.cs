0c6de19a8e310666a5e497b62c22969767d3e211
Румен Драганов <r.draganov@respect93.com>
2015-08-18 22:46:17 +0300

    Модул 03 Видео 04 - Работа с булеви променливи

---
+++ Modul03/BoolExplore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BoolExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// bool _b = true;
+			// bool _a = 5 > 10;
+			int[] _i = new int[] {22,12,34};
+
+			Console.Write("Въведете номер: ");
+			int _ui=Convert.ToInt32(Console.ReadLine());
+
+			bool _check01=_ui<=3;
+			bool _check02=_ui>0;
+
+
+			Console.WriteLine (_i[_ui-1]);
+		}
+	}
+}
