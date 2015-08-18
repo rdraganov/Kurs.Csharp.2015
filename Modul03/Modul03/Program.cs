d00573f551f2a79f99351bae766e39216710868a
Румен Драганов <r.draganov@respect93.com>
2015-08-18 22:13:32 +0300

    Модул 03 - видео 03 - Математически операции с double променливи

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -6,13 +6,26 @@ namespace Modul03
 	{
 		public static void Main (string[] args)
 		{
-		   double a = 0, b = 0;
+			double a = 0, b = 0, c = 0;
 		
 			Console.Write("> a="); a = Convert.ToDouble(Console.ReadLine());
 			Console.Write("> b="); b = Convert.ToDouble(Console.ReadLine());
 					
-			string _format = "#0.00 м3";
+			string _format = "#0.00";
 			Console.WriteLine ("\n\n> a / b = " + ( a / b ).ToString(_format));
+
+			//Степенуване
+			c=Math.Pow(a,b);
+			Console.WriteLine ("\n\n> a ^ b = " + ( c ).ToString(_format));
+
+			//Коренуване
+			c=Math.Sqrt(b);
+			Console.WriteLine ("\n\n> a ^ 1/2 = " + ( c ).ToString(_format));
+
+			//Закръгляване
+			c=Math.Round(a/b);
+			Console.WriteLine ("\n\n> a / b = " + ( c ).ToString());
+
 		}
 	}
 }
