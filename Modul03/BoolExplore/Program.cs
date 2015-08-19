cc6edf24e3684d2456597a02b64f4f69dc017439
Румен Драганов <r.draganov@respect93.com>
2015-08-19 23:06:27 +0300

    Модул 03 Видео 07 Работа с условен оператор if - 2

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -10,13 +10,16 @@ namespace BoolExplore
 			// bool _a = 5 > 10;
 			int[] _i = new int[] {22,12,34};
 
-			Console.Write("Въведете номер: ");
-			int _ui=Convert.ToInt32(Console.ReadLine());
+			Console.Write("Въведете индекс: ");
+			int _ui = 0;
+			string _input = "0";
+			_input=Console.ReadLine();
 
-			bool _check01=_ui<=_i.Length;
-			bool _check02=_ui>0;
+			bool _check01=int.TryParse(_input, out _ui);
+			bool _check02=_ui<=_i.Length;
+			bool _check03=_ui>0;
 
-			bool _exp01 = _check01 && _check02; 	//Логическо И *&&Ч
+			bool _exp01 = _check01 && _check02 && _check03; 	//Логическо И *&&Ч
 //			bool _exp02 = _check01 || _check02;		//Логическо ИЛИ *||*
 //			bool _exp03 = !_check01;				//Логическо НЕ *!*
 //
@@ -26,13 +29,17 @@ namespace BoolExplore
 
 			if (_exp01)
 			{
-				Console.WriteLine ("Елемент номер "+(int)_ui +" : "+ (int)_i[_ui-1]);
+				Console.WriteLine ("Елемент с индекс "+(int)_ui +" : "+ (int)_i[_ui-1]);
 			}
 			if (!_check01)
 			{
-				Console.WriteLine ("Индексът е прекалено голям \n\n\n");
+				Console.WriteLine ("Индексът трябва да е цяло число \n\n\n");
 			}
 			if (!_check02)
+			{
+				Console.WriteLine ("Индексът е прекалено голям \n\n\n");
+			}
+			if (!_check03 && _check01)
 			{
 				Console.WriteLine ("Индексът трябва да е по-голям от нула\n\n\n");
 			}
