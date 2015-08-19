63b75230c9a3176b07a293944ff44cd2f3ed636b
Румен Драганов <r.draganov@respect93.com>
2015-08-19 22:46:49 +0300

    Модул 03 Видео 06 Работа с условен оператор if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -17,13 +17,25 @@ namespace BoolExplore
 			bool _check02=_ui>0;
 
 			bool _exp01 = _check01 && _check02; 	//Логическо И *&&Ч
-			bool _exp02 = _check01 || _check02;		//Логическо ИЛИ *||*
-			bool _exp03 = !_check01;				//Логическо НЕ *!*
+//			bool _exp02 = _check01 || _check02;		//Логическо ИЛИ *||*
+//			bool _exp03 = !_check01;				//Логическо НЕ *!*
+//
+//			Console.WriteLine ("Логическо И: "+_exp01);
+//			Console.WriteLine ("Логическо ИЛИ: "+_exp02);
+//			Console.WriteLine ("Логическо НЕ за "+_check01+"   "+ _exp03+"\n\n");
 
-			Console.WriteLine ("Логическо И: "+_exp01);
-			Console.WriteLine ("Логическо ИЛИ: "+_exp02);
-			Console.WriteLine ("Логическо НЕ за "+_check01+"   "+ _exp03+"\n\n");
-
+			if (_exp01)
+			{
+				Console.WriteLine ("Елемент номер "+(int)_ui +" : "+ (int)_i[_ui-1]);
+			}
+			if (!_check01)
+			{
+				Console.WriteLine ("Индексът е прекалено голям \n\n\n");
+			}
+			if (!_check02)
+			{
+				Console.WriteLine ("Индексът трябва да е по-голям от нула\n\n\n");
+			}
 			//Console.WriteLine ("Елемент номер "+(int)_ui +" : "+ (int)_i[_ui-1]);
 
 
