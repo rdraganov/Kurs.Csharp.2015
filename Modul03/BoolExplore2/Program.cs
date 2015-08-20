2b153b0b4383fb5b596cd08a69a24071b279d6c1
Румен Драганов <r.draganov@respect93.com>
2015-08-20 22:07:20 +0300

    Модул 03 Видео 10 - Улавяне и обработка на грешки

--- Modul03/BoolExplore2/Program.cs
+++ Modul03/BoolExplore2/Program.cs
@@ -15,33 +15,42 @@ namespace BoolExplore2
 
 			bool _check01 = int.TryParse( _input, out _ui );
 
-			switch (_ui) {
-			case 1:
-				{
-					Console.Write ("1ви елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-			case 2:
-				{
-					Console.Write ("2ри елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-			case 3:
-				{
-					Console.Write ("1ти елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-			default :{
-					Console.WriteLine ("В масива няма елемент с този индекс.\n\n\nОпитайте с индекс между 1 и " 
-							+ Convert.ToString( _i.Length ) + "\n\n");
-					break;
-				}
+			try 
+			{
+				Console.Write("Елемент с номер "+_ui.ToString()+" е ");
+				Console.WriteLine(_i[_ui-1]);
+			}catch{
+				Console.WriteLine(" < недефиниран елемент >" );		
 			}
 
+			// Оператор switch
+//			switch (_ui) {
+//			case 1:
+//				{
+//					Console.Write ("1ви елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//			case 2:
+//				{
+//					Console.Write ("2ри елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//			case 3:
+//				{
+//					Console.Write ("1ти елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//			default :{
+//					Console.WriteLine ("В масива няма елемент с този индекс.\n\n\nОпитайте с индекс между 1 и " 
+//							+ Convert.ToString( _i.Length ) + "\n\n");
+//					break;
+//				}
+
+			}
 
 		}
-	}
+	
 }
