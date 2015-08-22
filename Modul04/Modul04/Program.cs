402da3c8e4e3bf96f74350aa2a5d03ad6e523f4e
Румен Драганов <r.draganov@respect93.com>
2015-08-22 23:22:29 +0300

    Модул 04 Видео03 - Примери за вложени цикли for

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -6,12 +6,22 @@ namespace Modul04
 	{
 		public static void Main (string[] args)
 		{
-			string[] _tbl = {"Генчо","Пенчо","Атанас","и аз"};
-			Console.WriteLine ("Име");
-			Console.WriteLine ("==========");
-			for (int i = 0; i < _tbl.Length ; i++) {
-				//Код на цикъла
-				Console.WriteLine (_tbl[i]);
+			string[,] _tbl = {
+				{"Генчо","Генчев","19"},
+				{"Цветан","Василев","55"},
+				{"Бойко","Борисов","60"},
+				{"Гергана","Маринова","33"},
+				{"Петя","Неделчева","25"},
+				{"Григор","Димитров","26"}
+			};
+			Console.WriteLine ("Име".PadRight(12)+"Фамилия".PadRight(12)+"Години".PadRight(12));
+			Console.WriteLine ("==============================");
+		for (int i = 0; i < _tbl.Length/3 ; i++) {
+//				//Код на цикъла
+				for (int j=0; j<3; j++){
+					Console.Write(_tbl[i,j].PadRight(12));
+				}
+				Console.WriteLine();
 			}
 			Console.WriteLine ("\n\n\n");
 			Console.ReadKey ();
