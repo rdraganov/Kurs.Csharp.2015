631a32340d20ec2bb11ecb9bcce5bf8857d41e76
Румен Драганов <r.draganov@respect93.com>
2015-08-23 22:34:31 +0300

    Модл 04 Видео 06 Пример с do...while

---
+++ Modul04/ListExplore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниции
+			string _userinput="";
+
+			//Потребителски вход
+			do {
+				Console.Write ("> ");
+				_userinput = Console.ReadLine ();
+
+				//Добавяне на елемент в списъка List
+
+				//Преглед на списъка List
+
+				//Размер на списъка List
+
+			} while(_userinput != "exit");
+		}
+	}
+}
