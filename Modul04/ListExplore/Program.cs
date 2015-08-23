a686327c08a2341787abdb6dfc8550dd9b9698f0
Румен Драганов <r.draganov@respect93.com>
2015-08-23 22:52:23 +0300

    Модл 04 Видео 07 Работа със List

--- Modul04/ListExplore/Program.cs
+++ Modul04/ListExplore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExplore
 {
@@ -8,6 +9,11 @@ namespace ListExplore
 		{
 			//Дефиниции
 			string _userinput="";
+			List<int> _list = new List<int> ();
+
+			_list.Add (14);
+			_list.Add (42);
+			_list.Add (33);
 
 			//Потребителски вход
 			do {
@@ -17,7 +23,15 @@ namespace ListExplore
 				//Добавяне на елемент в списъка List
 
 				//Преглед на списъка List
-
+				if (_userinput.Contains("show")){
+					Console.Write("List: ");
+					for (int i=0; i<_list.Count; i++)
+					{
+						Console.Write(_list[i]);
+						if (i < _list.Count -1) Console.Write(", ");
+					}
+					Console.WriteLine();
+				}
 				//Размер на списъка List
 
 			} while(_userinput != "exit");
