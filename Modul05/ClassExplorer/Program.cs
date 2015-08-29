51dbe6959df4b993f5d91d206ddc2fb6ee803276
Румен Драганов <r.draganov@respect93.com>
2015-08-29 22:56:04 +0300

    Модул 05 Видео 05 Използване на променливи в клас public/private

--- Modul05/ClassExplorer/Program.cs
+++ Modul05/ClassExplorer/Program.cs
@@ -5,17 +5,28 @@ namespace ClassExplorer
 	public class person
 	{
 		public string simpleString="Тестова променлива от клас";
-		public person()
+		private string hiddenString="";
+		public string user="";
+		public person(string _ini)
 		{
-			simpleString="Промяна при инициализация";
+			if (_ini == "password1")
+				hiddenString = "Потребител 1";
+			if (_ini == "password2")
+				hiddenString = "Потребител 2";
+			user = hiddenString;
+			if (hiddenString.Length > 0)
+				simpleString="Добре дошли отново, "+user+"!";
+			else
+				simpleString="Вие нямате достъп до програмата!";
 		}
 	}
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
+			Console.Write ("Парола: "); string _user = Console.ReadLine ();
 			//Дефиниране на клас
-			person _person = new person ();
+			person _person = new person (_user);
 
 			//използване на клас
 			Console.WriteLine (_person.simpleString);
