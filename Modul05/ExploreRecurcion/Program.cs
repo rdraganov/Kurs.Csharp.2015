a7177095cd382b50476b639006794823c6d2a3dc
Румен Драганов <r.draganov@respect93.com>
2015-09-01 21:39:13 +0300

    Модул 05 Видео 08 Рекурсии

---
+++ Modul05/ExploreRecurcion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExploreRecurcion
+{
+	
+	class MainClass
+	{
+		public static int fact(int _fact)
+		{
+			if (_fact<=1) return _fact;
+			else return _fact*fact(_fact-1);
+		}
+		public static void Main (string[] args)
+
+		{
+			Console.WriteLine(fact(3));
+			Console.WriteLine(fact(4));
+			Console.WriteLine(fact(5));
+			Console.WriteLine(fact(6));
+			Console.WriteLine(fact(7));
+
+		}
+	}
+}
