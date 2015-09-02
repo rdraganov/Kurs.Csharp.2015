d1bfa3c63ca3ef2d390f6aa4f6ad68bebe0c0e45
Румен Драганов <r.draganov@respect93.com>
2015-09-02 14:25:42 +0300

    Revert "Remove Module02 directory added by mistake"
    This reverts commit 8bf0285715541e2cc04d99b4ae6b8f397d625a94.

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на променливи
+			int a = 0, b = 0; 
+			int sum = 0;
+			string program_name = "Модул02 - примери";
+			string program_version = "1.0";
+            //Данни за програмата
+			Console.WriteLine(program_name);
+			Console.WriteLine("Версия "+program_version+"\n\n");
+
+			//Събиране на числа
+			a = 10;
+			b = 5;
+
+			sum = a + b;
+
+			//Писане в конзола
+			Console.Write ( a );
+			Console.Write (" + ");
+			Console.Write ( b );
+			Console.Write (" = ");
+			Console.WriteLine(sum);
+			//Допълнителни оператори
+			Console.WriteLine ("\nИзползване на +=");
+			a += b;
+			Console.WriteLine ( a );
+			Console.WriteLine ("\nИзползване на -=");
+			a -= b;
+			Console.WriteLine ( a );
+			Console.WriteLine ("\nИзползване на *=");
+			a *= b;
+			Console.WriteLine ( a );
+			Console.WriteLine ("\nИзползване на ++");
+			a++;
+			Console.WriteLine ( a );
+
+			a = 15;	b = 10;
+
+			Console.WriteLine ("\nСтандартно делене");
+			Console.WriteLine ( a/b );  //1
+
+			Console.WriteLine ("\nХващане на остатък");
+			Console.WriteLine ( a%b ); //5, 1.5
+
+
+		;
+		}
+	}
+}
